2770eeb624fff6026f865c8287c02f9c1fe796a6
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-04 09:35:48 +0900

    line-bot first

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,37 @@
+class LineBotController < ApplicationController
+  protect_from_forgery except: [:callback] #CSRF対策 :callbackアクションのみ無効化
+
+  def callback
+    body = request.body.read #readメソッドで文字列として読み出す
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          #返信メッセージ
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
+    #  p body
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+end
